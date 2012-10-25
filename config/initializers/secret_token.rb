078e8fd72039cf704e8437e39a29f1ac81c08e73
Phil Crissman <phil.crissman@aonhewitt.com>
2012-10-25 13:42:37 -0500

    A bright and shiny new example rails app with nothing in it.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MwsExampleApp::Application.config.secret_token = '12af02924dfd597b78af21e8c3286a6ee982633a052e5068d97364fbee794a8a58e4e6e58c39a5519715e1ff7f26f3f40b8c62053e9fc8fe7d076d43d456137a'
